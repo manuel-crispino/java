527640e96edace2efacacbf8cd6f46f0ce9c99f2
ManuelCrispino <manuel.barman96@gmail.com>
2026-09-20 12:33:27 +0200

    fix

--- java-ex/ex00/IntEqualityPrinter.java
+++ java-ex/ex00/IntEqualityPrinter.java
@@ -3,10 +3,10 @@ public class IntEqualityPrinter {
     public static void printEqual(int a, int b,int c ){
         if (a < 0 || b < 0 || c < 0)
             System.out.println("Invalid Value");
-        if ( a != b && b != c)
-            System.out.println("All Number are different");
-        if ( a == b && b == c)
-            System.out.println("All Number are equal");
+        else if ( a != b && b != c && a != c)
+            System.out.println("All numbers are different");
+        else if ( a == b && b == c)
+            System.out.println("All numbers re equal");
 	else 
             System.out.println("Neither all are equal or different");
     }
